69d58dfc13f9dbff154e476a5c4cc58812baa260
Paweł Słowik <pawel.slowik@zoho.com>
2017-01-04 15:30:23 +0100

    Add support for rxvt-unicode / urxvt

--- titles.plugin.zsh
+++ titles.plugin.zsh
@@ -16,6 +16,8 @@ function update_title() {
     print -n "\ek${(%)a}:${(%)2}\e\\"
   elif [[ "$TERM" =~ "xterm*" ]]; then
     print -n "\e]0;${(%)a}:${(%)2}\a"
+  elif [[ "$TERM" =~ "^rxvt-unicode.*" ]]; then
+    printf '\33]2;%s:%s\007' ${(%)a} ${(%)2}
   fi
 }
 
